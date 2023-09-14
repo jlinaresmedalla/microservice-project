71b478f36d778b2fef256a68770587fe19ccb3f7
Alvaro <jlinaresmedalla@gmail.com>
2023-09-13 19:18:06 -0500

    Finishing Service Registry server and load balancer

--- employee-service/src/main/java/com/example/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/com/example/employeeservice/service/APIClient.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.example.employeeservice.dto.DepartmentDto;
 
-@FeignClient(value = "DEPARMENT-SERVICE", url = "http://localhost:8080")
+//@FeignClient(value = "DEPARTMENT-SERVICE", url = "http://localhost:8080")
+@FeignClient(name = "DEPARTMENT-SERVICE") // Load balancer embeded will be used to call the service
 public interface APIClient {
 
     @GetMapping("/{id}")
     DepartmentDto getDepartmentById(@PathVariable Long id);
-
-
 
 }
